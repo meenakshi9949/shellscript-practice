9cab33155b2b4e0269dcc3abded1c963001d54d6
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-23 16:39:31 +0530

    shell

---
+++ 9-mysql-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then 
+echo "error:: please run script with root"
+exit 1
+else 
+echo "you are root user"
+fi 
+
+yum install mysql -y
+
+if [ $? -ne 0 ] 
+then 
+echo "error:: installing mysql is failure"
+exit 1
+else 
+echo "installing mysql is success"
+fi
+ 
+yum install git -y 
+
+if [ $? -ne 0 ] 
+then 
+echo "error:: installing mysql is failure"
+exit 1
+else 
+echo "installing mysql is success"
+fi
